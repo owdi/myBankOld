3c8678cbf502a0e6151bc20117d4da1eaf40f6e7
olivier dupre <olivier.dupre.pro@gmail.com>
2017-11-29 21:08:51 +0100

    add bert and iron router

--- client/templates/user_connexion/js/user_connexion.js
+++ client/templates/user_connexion/js/user_connexion.js
@@ -1,29 +1,43 @@
 if (Meteor.isClient) {
-    Template.formRegister.events({
-        'submit form': function(event) {
-            event.preventDefault();
-            var userEmail = event.target.registerEmail.value;
-            var userPassword = event.target.registerPassword.value;
-            Accounts.createUser({
-                email: userEmail,
-                password: userPassword
-            });
-        }
-    });
+  Template.formRegister.events({
+    'submit form': function (event) {
+      event.preventDefault();
+      var userEmail = event.target.registerEmail.value;
+      var userPassword = event.target.registerPassword.value;
+      Accounts.createUser({
+        email: userEmail,
+        password: userPassword
+      });
+    }
+  });
 
-    Template.formLogin.events({
-        'submit form': function(event) {
-            event.preventDefault();
-            var userEmail = event.target.loginEmail.value;
-            var userPassword = event.target.loginPassword.value;
-            Meteor.loginWithPassword(userEmail, userPassword);
-        }
-    });
+  Template.formLogin.events({
+    'submit form': function (event) {
+      event.preventDefault();
+      var userEmail = event.target.loginEmail.value;
+      var userPassword = event.target.loginPassword.value;
 
-    Template.dashboard.events({
-        'click .logout': function(event){
-            event.preventDefault();
-            Meteor.logout();
+      Meteor.loginWithPassword(userEmail, userPassword, function (err) {
+
+        if (err) {
+          Bert.alert({
+            message: 'Le mot de passe ou l\'email ne sont pas valides',
+            type: 'danger'
+          });
+        }else{
+          console.log('toto');
+            Session.set('isConnected', true);
+            Router.go('/dashboard');
         }
-    });
+      });
+    }
+
+  });
+
+  Template.dashboard.events({
+    'click .logout': function (event) {
+      event.preventDefault();
+      Meteor.logout();
+    }
+  });
 }
